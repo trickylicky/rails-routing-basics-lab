2f4fbaffc18e8d083fd828657c88df2d68e00460
Tricky Licky <wangombevictor5@gmail.com>
2022-11-21 21:51:29 +0300

    complete assignment

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,13 @@
+class StudentsController < ApplicationController
+
+    def index
+        students = Student.all
+        render json: students
+
+    end
+
+    def grades
+        grades = Student.order(grade: :desc)
+        render json: grades
+    end
+end
